d7cfb2a314fa96278450ca081142e5f4cceb1e1a
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-09-21 19:41:08 +0530

    made changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,209 @@
-import Image from "next/image";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+    <>
+      <div className="bg-red-500 w-screen h-screen">
+        {/* Top Bar */}
+        <div className="bg-black w-full h-15 border-b border-gray-500 flex items-center px-2 gap-3">
+          <div className="w-10 h-full flex items-center justify-center mt-1">
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+          <a href="https://github.com/navaneethk99">
+            <p className="font-bold text-md">navaneethk99</p>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="flex h-full w-full">
+          {/* Left Sidebar */}
+          <div className="bg-[#0D1117] w-120 h-full flex flex-col items-center">
+            <div className="w-75 h-75 rounded-full bg-white mt-10">
+              <img
+                src="/images/88372623.jpeg"
+                className="w-full h-full rounded-full"
+                alt="profile"
+              />
+            </div>
+
+            <div className="w-full">
+              <h1 className="mt-5 ml-10 text-xl text-gray-400">navaneethk99</h1>
+              <div className="mt-2 ml-10 text-sm text-gray-500 flex">
+                <b>
+                  <p className="text-white mr-1">1</p>
+                </b>{" "}
+                follower{" "}
+                <b>
+                  <p className="text-white mr-1 ml-1">· 3</p>
+                </b>{" "}
+                following
+              </div>
+
+              <p className="ml-10 text-sm mt-5 font-bold">
+                Vellore Institute of Technology
+              </p>
+              <p className="ml-10 text-sm mt-1 font-bold">Vellore</p>
+              <p className="ml-10 text-sm mt-1 font-bold">
+                BTech Computer Science Engineering
+              </p>
+              <p className="ml-10 text-xs text-gray-500">
+                Artificial Intelligence and Data Engineering
+              </p>
+
+              {/* Organisations Section */}
+              <div className="w-full border-t border-gray-700 ml-10 mt-5">
+                <h1 className="mt-5 font-bold text-lg">Organisations</h1>
+
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md">
+                    <img
+                      src="/images/1_gpyPa2lnqhHz5GeoUq-vgQ.png"
+                      className="w-full h-full rounded-md"
+                    ></img>
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Association for Computing Machinery
+                    </p>
+                    <p className="font-bold">May 2025 - Present</p>
+                  </div>
+                </div>
+
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md">
+                    <img
+                      src="/images/stratign_logo.jpeg"
+                      className="w-full h-full rounded-md"
+                    ></img>
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Stratign (Internship) - Software Developer
+                    </p>
+                    <p className="font-bold">June 2025 - July 2025</p>
+                  </div>
+                </div>
+
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md bg-white p-1">
+                    <img
+                      src="/images/Vellore_Institute_of_Technology_seal_2017.svg.png"
+                      className="w-full h-full"
+                    ></img>
+                  </div>
+                  <div>
+                    <p className="text-sm">Vellore Institute of Technology</p>
+                    <p className="font-bold">July 2024 - Present</p>
+                  </div>
+                </div>
+
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md bg-white">
+                    <img
+                      src="/images/delhi_public_school_ghaziabad_vasundhara_logo.jpeg"
+                      className="w-full h-full rounded-md"
+                    ></img>
+                  </div>
+                  <div>
+                    <p className="text-sm">Delhi Public School Vasundhara</p>
+                    <p className="font-bold">April 2010 - April 2024</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Main Content */}
+          <div className="bg-[#0D1117] w-full h-full pt-10 px-8 pr-10">
+            <h1 className="font-bold">Popular Repositories</h1>
+
+            {/* Repo Grid */}
+            <div className="grid grid-cols-2 gap-5 mt-4">
+              <div className="w-full h-full rounded-md border border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">codigo</h1>
+                <p className="text-sm text-gray-400">
+                  Codigo is a mobile app that makes learning to code simple and
+                  engaging. Designed to be the “Duolingo for programming,” it
+                  helps beginners step into the world of computer programming
+                  through lessons, interactive challenges, and gamified progress
+                  tracking.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    React Native
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Python
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Fast API
+                  </div>
+                  <div className="bg-blue-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    MongoDB
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full h-full rounded-md border border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">
+                  cryptic hunt dashboard
+                </h1>
+                <p className="text-sm text-gray-400">
+                  Cryptic Hunt Dashboard is an internal moderation tool. It
+                  enables moderators to manage and oversee gameplay for the
+                  Cryptic Hunt event, handling around 400 concurrent players in
+                  real time. The dashboard ensures a smooth and fair experience
+                  for all participants.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Nextjs
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Prisma
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    TailwindCSS
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full h-full rounded-md border border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">
+                  cryptic hunt rsvp
+                </h1>
+                <p className="text-sm text-gray-400">
+                  Cryptic Hunt RSVP is a participant management tool designed to
+                  handle signups and event onboarding smoothly for the Cryptic
+                  Hunt. It ensures seamless registration and fair participation.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Nextjs
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Framer Motion
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    TailwindCSS
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full h-full rounded-md border border-gray-600 p-4"></div>
+            </div>
+
+            {/* About Me */}
+            <h1 className="font-bold mt-10">Description</h1>
+            <div className="mt-5 border rounded-md border-gray-600 p-4">
+              about me
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Page;
