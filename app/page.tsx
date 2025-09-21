0a1030489172d079ece4cc0e7fa2cc6136ad610a
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-09-21 18:59:59 +0530

    my repos almost finished

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,15 @@ const page = () => {
         </div>
         <div className="flex h-full w-full">
           <div className="bg-gray-900 w-120 h-full flex flex-col items-center">
-            <div className="w-75 h-75 rounded-full bg-white mt-10"></div>
+            <div className="w-75 h-75 rounded-full bg-white mt-10">
+              <img
+                src="/images/88372623.jpeg"
+                className="w-full h-full rounded-full"
+              ></img>
+            </div>
             <div className="w-full">
               <h1 className="mt-5 ml-10 text-xl text-gray-400">navaneethk99</h1>
-              <p className="mt-2 ml-10 text-sm text-gray-500 flex">
+              <div className="mt-2 ml-10 text-sm text-gray-500 flex">
                 <b>
                   <p className="text-white mr-1">1</p>
                 </b>{" "}
@@ -21,7 +26,7 @@ const page = () => {
                   <p className="text-white mr-1 ml-1">· 3</p>
                 </b>{" "}
                 following
-              </p>
+              </div>
               <p className="ml-10 text-sm mt-5 font-bold">
                 Vellore Institute of Technology
               </p>
@@ -52,10 +57,104 @@ const page = () => {
                     <p className="font-bold">June 2025 - July 2025</p>
                   </div>
                 </div>
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md bg-white">logo</div>
+                  <div>
+                    <p className="text-sm">Vellore Institute of Technology</p>
+                    <p className="font-bold">July 2024 - Present</p>
+                  </div>
+                </div>
+                <div className="mt-2 w-full flex gap-2">
+                  <div className="w-10 h-10 rounded-md bg-white">logo</div>
+                  <div>
+                    <p className="text-sm">
+                      Delhi Public School Ghaziabad Vasundhara
+                    </p>
+                    <p className="font-bold">April 2010 - April 2024</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="bg-gray-900 w-full h-full">popular repositories</div>
+          <div className="bg-gray-900 w-full h-full pt-10 pl-2">
+            <h1 className="font-bold">Popular Repositories </h1>
+            <div className="grid grid-cols-2 gap-5 pr-35 mt-4">
+              <div className="w-full h-full rounded-md border-1 border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">codigo</h1>
+                <p className="text-sm text-gray-400">
+                  Codigo is a mobile app that makes learning to code simple and
+                  engaging. Designed to be the “Duolingo for programming,” it
+                  helps beginners step into the world of computer programming
+                  through lessons, interactive challenges, and gamified progress
+                  tracking.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    React Native
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Python
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Fast API
+                  </div>
+                  <div className="bg-blue-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    MongoDB
+                  </div>
+                </div>
+              </div>
+              <div className="w-full h-full rounded-md border-1 border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">
+                  cryptic hunt dashboard
+                </h1>
+                <p className="text-sm text-gray-400">
+                  Cryptic Hunt Dashboard is an internal moderation tool. It
+                  enables moderators to manage and oversee gameplay for the
+                  Cryptic Hunt event, handling around 400 concurrent players in
+                  real time. The dashboard ensures a smooth and fair experience
+                  for all participants.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    React Nextjs
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Prisma
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    TailwindCSS
+                  </div>
+                </div>
+              </div>
+              <div className="w-full h-full rounded-md border-1 border-gray-600 p-4">
+                <h1 className="font-bold text-md text-blue-500 mb-2">
+                  cryptic hunt rsvp
+                </h1>
+                <p className="text-sm text-gray-400">
+                  Cryptic Hunt Dashboard is an internal moderation tool. It
+                  enables moderators to manage and oversee gameplay for the
+                  Cryptic Hunt event, handling around 400 concurrent players in
+                  real time. The dashboard ensures a smooth and fair experience
+                  for all participants.
+                </p>
+                <div className="flex gap-2 mt-2">
+                  <div className="bg-yellow-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    React Nextjs
+                  </div>
+                  <div className="bg-green-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    Framer Motion
+                  </div>
+                  <div className="bg-red-500 w-fit rounded-full px-2 py-1 text-xs font-bold">
+                    TailwindCSS
+                  </div>
+                </div>
+              </div>
+              <div className="w-full h-full rounded-md border-1 border-gray-600 p-4"></div>
+            </div>
+            <div className="mt-5 border-1 rounded-md h-full border-gray-600 mr-35">
+              about me
+            </div>
+          </div>
         </div>
       </div>
     </>
